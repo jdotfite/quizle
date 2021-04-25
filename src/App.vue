080a975835ddbe4b7a7e53cbc98bcf82235dd850
jdotfite <jdotfite@gmail.com>
2021-04-25 13:20:14 -0400

    BootstrapVue Boilerplate

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container>
+    <h1>Hello, Bootstrap-Vue</h1>
+    <b-btn 
+      variant="primary"
+      @click="showAlert = true"
+    >
+      Click
+    </b-btn>
+    <b-alert 
+      v-bind:show="showAlert"
+      variant="success"
+    >
+      You clicked the button!
+    </b-alert>
+  </b-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    showAlert: false
+  })
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
